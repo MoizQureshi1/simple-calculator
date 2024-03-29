f3cf40790af4ad26f041e80218f936d7f23793bf
MoizQureshi1 <159722436+MoizQureshi1@users.noreply.github.com>
2024-03-30 01:11:19 +0500

    tweak tha text and added console at the end

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-    {message: "Enter first number ", type: "number", name: "firstNumber"},
-    {message: "Enter second number ", type: "number", name: "secondNumber"},
+    {message: "Enter your first number ", type: "number", name: "firstNumber"},
+    {message: "Enter your second number ", type: "number", name: "secondNumber"},
     {
-        message: "Select one of the Operators to Perform Operation",
+        message: "Select one of the operators to perform operation",
         type: "list", 
         name: "operator", 
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
@@ -29,3 +29,5 @@ else if(answer.operator === "Division"){
 else{
     console.log("Please select valid operator");
 };
+
+console.log("THE END");
